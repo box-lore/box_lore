ef6e6316152ee2a455633212f852742ef21f3bf3
Dylan Drumm <dyl.drumm@gmail.com>
2023-03-05 16:54:12 -0500

    Added Axios package
    Added Axios package to Express server for API calls

--- server/index.js
+++ server/index.js
@@ -19,3 +19,22 @@ app.get('/', (req, res) => {
 app.listen(3001, () => {
     console.log("Server running on port 3001");
 })
+
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://v1.basketball.api-sports.io/seasons',
+  headers: {
+    'x-rapidapi-key': 'e818b2873f73f8796601575c1afa2ae1',
+    'x-rapidapi-host': 'v1.basketball.api-sports.io'
+  }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
