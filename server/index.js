56b15f57b8ca8c68001a1f7e4fa6f4a68d0c593d
Dylan Drumm <dyl.drumm@gmail.com>
2023-03-15 16:14:03 -0400

    More small changes

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const getAPIs = require('get-apis')
+
 const mysql = require('mysql')
 const express = require ('express')
 const bodyParser = require('body-parser')
@@ -26,28 +28,70 @@ app.listen(3001, () => {
 
 var axios = require('axios');
 
-const DYL_API = `${process.env.REACT_APP_DYL_API_KEY}`;
-console.log(process.env.REACT_APP_DYL_API_KEY);
+const DYL_API = `${process.env.DYL_API_KEY}`;
+const RAPID_API = `${process.env.DYL_NBA_API}`
 
-var config = {
-  method: 'get',
-  url: 'https://v1.basketball.api-sports.io/seasons',
-  headers: {
-    'x-rapidapi-key': DYL_API,
-    'x-rapidapi-host': 'v1.basketball.api-sports.io'
-  }
-};
+// -- API - Basketball --
 
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-  var json = JSON.stringify(response.data);
-  fs.writeFile('../client/jsons/test.json', json, 'utf8', (err) => {
-    // In case of a error throw err.
-    if (err) throw err;
-});
+// var api_sports_call = {
+//   method: 'get',
+//   url: 'https://v1.basketball.api-sports.io/seasons',
+//   headers: {
+//     'x-rapidapi-key': DYL_API,
+//     'x-rapidapi-host': 'v1.basketball.api-sports.io'
+//   }
+// };
 
-})
-.catch(function (error) {
-  console.log(error);
-});
+// axios(api_sports_call)
+// .then(function (response) {
+//   console.log(JSON.stringify(response.data));
+//   var json = JSON.stringify(response.data);
+//   fs.writeFile('../client/jsons/test.json', json, 'utf8', (err) => {
+//     // In case of a error throw err.
+//     if (err) throw err;
+// });
+
+// -- API - NBA --
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://api-nba-v1.p.rapidapi.com/seasons',
+//   headers: {
+//     'X-RapidAPI-Key': NBA_API,
+//     'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+//   var json = JSON.stringify(response.data);
+//   fs.writeFile('../client/jsons/nba.json', json, 'utf8', (err) => {
+//     // In case of a error throw err.
+//     if (err) throw err;
+// });
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+// -- API - Free NBA --
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://free-nba.p.rapidapi.com/stats',
+//   params: {game_ids: 'game_ids', per_page: '100', page: '0'},
+//   headers: {
+//     'X-RapidAPI-Key': RAPID_API,
+//     'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+//   var json = JSON.stringify(response.data);
+//   fs.writeFile('../client/jsons/stats.json', json, 'utf8', (err) => {
+//     // In case of a error throw err.
+//     if (err) throw err;
+//   });
+// }).catch(function (error) {
+// 	console.error(error);
+// });
