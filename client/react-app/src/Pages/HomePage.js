5ad41d6b92828a0c95f4d2c854b6a98a2abdb475
TyAbra <74419488+TyAbra@users.noreply.github.com>
2023-04-05 21:55:00 -0400

    News Component

--- client/react-app/src/Pages/HomePage.js
+++ client/react-app/src/Pages/HomePage.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 
 import StandingsTable from '../Components/StandingsTable/StandingsTable.js';
 import UpcomingGames from '../Components/UpcomingGames/UpcomingGames.js';
+import News from '../Components/News/News.js';
 
 function HomePage() {
     return (
@@ -11,8 +12,10 @@ function HomePage() {
       <div className="StandingsTable">
         <StandingsTable />
         <p/>
+        <News />
         <UpcomingGames />
       </div>
+      
     </div>
     );
 }
